b90994dbf6f2f76a3116a9a825cbfc084fb2227c
Rey0x <67438294+Bluesfire1467@users.noreply.github.com>
2026-02-26 22:06:36 -0600

    despues de la tarea

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import axios, { type AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly pokemonService: PokemonService) {}
+
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
@@ -14,6 +17,10 @@ export class SeedService {
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
+
+      console.log(no);
+
+      this.pokemonService.create({ name, no });
     });
 
     return data.results;
